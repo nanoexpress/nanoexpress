b7a47215c40493a5e90f623a3dfb4389368a5869
Davlat Shavkatov <dalisoft@mail.ru>
2019-05-26 22:53:36 +0500

    improve performance

--- src/middlewares/http.js
+++ src/middlewares/http.js
@@ -1,53 +1,52 @@
 import { http } from '../handler';
 
-export default async (path = '/*', fns, config) => {
+export default (path = '/*', fns, config) => {
   if (typeof path === 'function') {
     fns.unshift(path);
     path = '/*';
   }
   const lastFn = fns[fns.length - 1];
-  return (req, res) =>
-    http(
-      path,
-      async (req, res) => {
-        let result;
-        for await (const fn of fns) {
-          // Add Express-like middlewares support
-          if (typeof fn === 'function') {
-            if (!fn.then && fn.constructor.name !== 'AsyncFunction') {
-              if (fn === lastFn) {
-                result = fn(req, res, config);
-              } else {
-                result = await new Promise((resolve, reject) => {
-                  const next = (err, done) => {
-                    if (err) {
-                      console.error(
-                        '[Server]: Error - Middleware crashed or failed',
-                        err
-                      );
-                      reject(err);
-                    } else {
-                      resolve(done);
-                    }
-                  };
-                  fn(req, res, next, config);
-                });
-              }
-            } else if (fn.then || fn.constructor.name === 'AsyncFunction') {
-              result = await fn(req, res, config).catch((err) => {
-                console.error(
-                  '[Server]: Error - Middleware crashed or failed',
-                  err
-                );
+  return http(
+    path,
+    async (req, res) => {
+      let result;
+      for await (const fn of fns) {
+        // Add Express-like middlewares support
+        if (typeof fn === 'function') {
+          if (!fn.then && fn.constructor.name !== 'AsyncFunction') {
+            if (fn === lastFn) {
+              result = fn(req, res, config);
+            } else {
+              result = await new Promise((resolve, reject) => {
+                const next = (err, done) => {
+                  if (err) {
+                    console.error(
+                      '[Server]: Error - Middleware crashed or failed',
+                      err
+                    );
+                    reject(err);
+                  } else {
+                    resolve(done);
+                  }
+                };
+                fn(req, res, next, config);
               });
             }
+          } else if (fn.then || fn.constructor.name === 'AsyncFunction') {
+            result = await fn(req, res, config).catch((err) => {
+              console.error(
+                '[Server]: Error - Middleware crashed or failed',
+                err
+              );
+            });
           }
         }
-        if (result === undefined || result === null) {
-          result = '[Server]: Error - Response not found';
-        }
-        return result;
-      },
-      config
-    )(req, res);
+      }
+      if (result === undefined || result === null) {
+        result = '[Server]: Error - Response not found';
+      }
+      return result;
+    },
+    config
+  );
 };
