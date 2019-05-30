b6badef93b4b71f4858875abbfe3799d3d644fa5
Davlat Shavkatov <dalisoft@mail.ru>
2019-05-30 16:03:45 +0500

    trying to reduce complexity

--- src/helpers/prepare-validation.js
+++ src/helpers/prepare-validation.js
@@ -1,6 +1,6 @@
 import fastJson from 'fast-json-stringify';
 
-const validationMethods = ['query', 'params', 'headers', 'body'];
+const validationMethods = ['response', 'query', 'params', 'headers', 'body'];
 const validationSchema = {
   type: 'array',
   items: {
@@ -18,23 +18,22 @@ const validationSchema = {
 export default (ajv, schema) => {
   const validation = [];
   let validationStringify;
-  if (typeof schema === 'object' && schema) {
-    if (typeof schema.response === 'object') {
-      schema.response = fastJson(schema.response);
-    }
-    if (ajv) {
-      validationMethods.forEach((type) => {
-        const _schema = schema[type];
-        if (typeof _schema === 'object') {
+  if (ajv) {
+    validationMethods.forEach((type) => {
+      const _schema = schema[type];
+      if (typeof _schema === 'object') {
+        if (type === 'response') {
+          schema[type] = fastJson(_schema);
+        } else {
           const validator = ajv.compile(_schema);
           schema[type] = validator;
           validation.push({ type, validator });
+          if (!validationStringify) {
+            validationStringify = fastJson(validationSchema);
+          }
         }
-      });
-      if (validation.length > 0) {
-        validationStringify = fastJson(validationSchema);
       }
-    }
+    });
   }
   return {
     validation,
