1d35c159d8edd0a08df059084ca7982132567d91
Davlat Shavkatov <dalisoft@mail.ru>
2019-05-30 16:08:28 +0500

    reduce complexity

--- src/normalizers/cookies.js
+++ src/normalizers/cookies.js
@@ -2,14 +2,17 @@ const { parse } = require('cookie');
 
 module.exports = (req, cookies = {}) => {
   const { headers } = req;
+  const headerCookie = headers && headers.cookie;
 
-  if (cookies && headers && headers.cookie) {
-    const parsedCookie = parse(headers.cookie);
-    for (const cookie in parsedCookie) {
-      cookies[cookie] = parsedCookie[cookie];
+  if (headerCookie) {
+    if (cookies) {
+      const parsedCookie = parse(headerCookie);
+      for (const cookie in parsedCookie) {
+        cookies[cookie] = parsedCookie[cookie];
+      }
+    } else if (!cookies) {
+      cookies = parse(headerCookie);
     }
-  } else if (!cookies && headers && headers.cookie) {
-    cookies = parse(headers.cookie);
   }
   return cookies;
 };
