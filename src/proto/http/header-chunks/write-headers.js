dddb2b7c74c4f467de1b3d1c1733bcddd801df9d
Davlat Shavkatov <dalisoft@mail.ru>
2019-06-01 19:08:32 +0500

    typo fix

--- src/proto/http/header-chunks/write-headers.js
+++ src/proto/http/header-chunks/write-headers.js
@@ -1,7 +1,7 @@
 export default function writeHeaders(headers) {
   for (const header in headers) {
     const value = headers[header];
-    if (value !== undefined && value !== null) {
+    if (value) {
       if (value.splice) {
         this.writeHeaderValues(header, value);
       } else {
