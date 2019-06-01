d3fc6aaf1edc69c1e471502d245e14f819cb62db
Davlat Shavkatov <dalisoft@mail.ru>
2019-06-01 19:15:20 +0500

    reduce complexity

--- src/proto/http/polyfill-chunks/redirect.js
+++ src/proto/http/polyfill-chunks/redirect.js
@@ -1,20 +1,28 @@
+const HTTP_PREFIX = 'http://';
+const HTTPS_PREFIX = 'https://';
+
+const normalizeLocation = (path, config, host) => {
+  if (path.indexOf('http') === -1) {
+    if (path.indexOf('/') === -1) {
+      path = '/' + path;
+    }
+    const httpHost = (config && config.host) || host;
+    path =
+      config && config.https ? HTTPS_PREFIX : HTTP_PREFIX + httpHost + path;
+  }
+  return path;
+};
+
 export default function redirect(code, path) {
   const { __request, config } = this;
   const host = __request && __request.headers && __request.headers.host;
-
-  const httpHost = (config && config.host) || host;
 
   if (!path) {
     path = code;
     code = 301;
   }
 
-  if (!path.startsWith('http')) {
-    if (!path.startsWith('/')) {
-      path = '/' + path;
-    }
-    path = config && config.https ? 'https://' : 'http://' + httpHost + path;
-  }
+  path = normalizeLocation(path, config, host);
 
   this.writeHead(code, { Location: path });
   this.end();
