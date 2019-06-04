611c15fe9bc67bb50ceb7f70fb22bfc6665db68b
Davlat Shavkatov <dalisoft@mail.ru>
2019-06-04 16:53:25 +0500

    fixing typescript declaration

--- nanoexpress.d.ts
+++ nanoexpress.d.ts
@@ -9,126 +9,157 @@ import { Ajv, Options as AjvOptions } from 'ajv';
 
 declare namespace nanoexpress {
   export interface AppOptions extends AppOptionsBasic {
-  https?: {
-    key_file_name: string;
-    cert_file_name: string;
-    passphare: string;
-  };
-  ajv?: AjvOptions,
-  configureAjv(ajv: Ajv): Ajv
-}
+    https?: {
+      key_file_name: string;
+      cert_file_name: string;
+      passphare: string;
+    };
+    ajv?: AjvOptions;
+    configureAjv(ajv: Ajv): Ajv;
+  }
 
-export interface HttpRequestHeaders {
-  [key: string]: string;
-}
-export interface HttpRequestQueries {
-  [key: string]: string;
-}
-export interface HttpRequestParams {
-  [key: string]: string;
-}
-export interface HttpRequestBody {
-  [key: string]: string | object | any[];
-}
-export interface HttpRequestCookies {
-  [key: string]: string;
-}
+  export interface HttpRequestHeaders {
+    [key: string]: string;
+  }
+  export interface HttpRequestQueries {
+    [key: string]: string;
+  }
+  export interface HttpRequestParams {
+    [key: string]: string;
+  }
+  export interface HttpRequestBody {
+    [key: string]: string | object | any[];
+  }
+  export interface HttpRequestCookies {
+    [key: string]: string;
+  }
 
-export interface WebSocket extends WebSocketBasic {
-  on(name: string, listener: Function);
-  once(name: string, listener: Function);
-  off(name: string, listener?: Function);
-  emit(name: string, ...args: any[])
-}
-export interface HttpRequest extends HttpRequestBasic {
-  path: string;
-  url: string;
-  headers: HttpRequestHeaders;
-  cookies: HttpRequestCookies;
-  query: HttpRequestQueries;
-  params: HttpRequestParams;
-  body?: HttpRequestBody;
-  private __response: HttpResponse;
-}
+  export interface WebSocket extends WebSocketBasic {
+    on(name: string, listener: Function);
+    once(name: string, listener: Function);
+    off(name: string, listener?: Function);
+    emit(name: string, ...args: any[]);
+  }
+  export interface HttpRequest extends HttpRequestBasic {
+    path: string;
+    url: string;
+    headers: HttpRequestHeaders;
+    cookies: HttpRequestCookies;
+    query: HttpRequestQueries;
+    params: HttpRequestParams;
+    body?: HttpRequestBody;
+    __response?: HttpResponse;
+  }
 
-export interface CookieOptions {
-  httpOnly?: boolean;
-  secure?: boolean;
-  maxAge?: number;
-  path?: string;
-  domain?: string;
-  signed?: boolean;
-  expires?: number | string;
-}
-export interface HttpResponse extends HttpResponseBasic {
-  status(code: number): HttpResponse;
-  setHeader(key: string, value: string | number): HttpResponse;
-  hasHeader(key: string): HttpResponse;
-  removeHeader(key: string): HttpResponse;
-  applyHeadersAndStatus(): HttpResponse;
-  setHeaders(headers: HttpRequestHeaders): HttpResponse;
-  writeHeaderValues(name: string, value: string[]): HttpResponse;
-  writeHeaders(headers: HttpRequestHeaders): HttpResponse;
-  writeHead(code: number; headers: HttpRequestHeaders): HttpResponse;
-  redirect(code: number | string, path?: string): HttpResponse;
-  send(result: string | object | array): HttpResponse;
-  json(result: object | array): HttpResponse;
-  cork(result: string | object | array): HttpResponse;
-  setCookie(key: string, value: string, options?: CookieOptions): HttpResponse;
-  cookie(key: string, value: string, options?: CookieOptions): HttpResponse;
-  hasCookie(key: string): HttpResponse;
-  removeCookie(key: string, options?: CookieOptions): HttpResponse;
-  __request?: HttpRequest
-}
+  export interface CookieOptions {
+    httpOnly?: boolean;
+    secure?: boolean;
+    maxAge?: number;
+    path?: string;
+    domain?: string;
+    signed?: boolean;
+    expires?: number | string;
+  }
+  export interface HttpResponse {
+    status(code: number): HttpResponse;
+    setHeader(key: string, value: string | number): HttpResponse;
+    hasHeader(key: string): HttpResponse;
+    removeHeader(key: string): HttpResponse;
+    applyHeadersAndStatus(): HttpResponse;
+    setHeaders(headers: HttpRequestHeaders): HttpResponse;
+    writeHeaderValues(name: string, value: string[]): HttpResponse;
+    writeHeaders(headers: HttpRequestHeaders): HttpResponse;
+    writeHead(code: number, headers: HttpRequestHeaders): HttpResponse;
+    redirect(code: number | string, path?: string): HttpResponse;
+    send(result: string | object | any[]): HttpResponse;
+    json(result: object | any[]): HttpResponse;
+    cork(result: string | object | any[]): HttpResponse;
+    setCookie(
+      key: string,
+      value: string,
+      options?: CookieOptions
+    ): HttpResponse;
+    cookie(key: string, value: string, options?: CookieOptions): HttpResponse;
+    hasCookie(key: string): HttpResponse;
+    removeCookie(key: string, options?: CookieOptions): HttpResponse;
+    __request?: HttpRequest;
+  }
 
-export interface AppRoute {
-  get(req: HttpRequest, res: HttpResponse): any
-  post(req: HttpRequest, res: HttpResponse): any
-  put(req: HttpRequest, res: HttpResponse): any
-  patch(req: HttpRequest, res: HttpResponse): any
-  head(req: HttpRequest, res: HttpResponse): any
-  delete(req: HttpRequest, res: HttpResponse): any
-  trace(req: HttpRequest, res: HttpResponse): any
-  ws(req: HttpRequest, ws: WebSocket): any
-}
-export interface AppRoute {
-  callback: Function<HttpRequest, HttpResponse>;
-  middlewares?: Function<HttpRequest, HttpResponse, Function>[];
-  schema?: object;
-}
-export interface AppRoutes {
-  [key: string]: AppRoute | AppRoute | object;
-}
+  type HttpRoute = (
+    req: HttpRequest,
+    res: HttpResponse,
+    next?: Function,
+    config?: object
+  ) => any;
+  type WsRoute = (req: HttpRequest, ws: WebSocket) => any;
 
-export interface AppConfig {
-  config: {
-    set(key: string, value: any): void,
-    get(key: string): any;
+  export interface AppRoute {
+    get: HttpRoute;
+    post: HttpRoute;
+    put: HttpRoute;
+    patch: HttpRoute;
+    head: HttpRoute;
+    delete: HttpRoute;
+    options: HttpRoute;
+    trace: HttpRoute;
+    any: HttpRoute;
+    ws: WsRoute;
+  }
+
+  interface Schema {
+    [key: string]: string | Schema;
+  }
+  interface MiddlewareOption {
+    schema?: Schema;
+    isRaw?: boolean;
+  }
+
+  export interface AppRoute {
+    callback<HttpRequest, HttpResponse>(
+      req: HttpRequest,
+      res: HttpResponse
+    ): any;
+    middlewares?: HttpRoute[];
+    schema?: Schema;
+  }
+  export interface AppRoutes {
+    [key: string]: AppRoute | AppRoutes | Function;
+  }
+
+  export interface AppConfig {
+    config: {
+      set(key: string, value: any): void;
+      get(key: string): any;
+    };
+  }
+
+  type Middleware = MiddlewareOption | HttpRoute;
+
+  export interface nanoexpressApp {
+    host: string | null;
+    port: number | null;
+    address: string;
+    static(route: string, folderPath: string): nanoexpressApp;
+    use(path: string | Middleware, ...fns: Middleware[]): nanoexpressApp;
+    get(path: string, ...fns: Middleware[]): nanoexpressApp;
+    post(path: string, ...fns: Middleware[]): nanoexpressApp;
+    put(path: string, ...fns: Middleware[]): nanoexpressApp;
+    patch(path: string, ...fns: Middleware[]): nanoexpressApp;
+    del(path: string, ...fns: Middleware[]): nanoexpressApp;
+    options(path: string, ...fns: Middleware[]): nanoexpressApp;
+    head(path: string, ...fns: Middleware[]): nanoexpressApp;
+    trace(path: string, ...fns: Middleware[]): nanoexpressApp;
+    any(path: string | Middleware, ...fns: Middleware[]): nanoexpressApp;
+    ws(path: string, ...fns: Middleware[]): nanoexpressApp;
+    listen(port: number, host?: string): Promise<nanoexpressApp>;
+    close(): boolean;
+    define(prefix: string, routes?: AppRoutes): nanoexpressApp;
+    config: AppConfig;
   }
 }
 
-export interface nanoexpressApp extends BasicApp {
-  host: string | null;
-  port: number | null;
-  address: string;
-  use(path: string | Function, ...fns?: Function<HttpRequest, HttpResponse>): nanoexpressApp;
-  get(path: string, ...fns: Function<HttpRequest, HttpResponse>[]): nanoexpressApp;
-  post(path: string, ...fns: Function<HttpRequest, HttpResponse>[]): nanoexpressApp;
-  put(path: string, ...fns: Function<HttpRequest, HttpResponse>[]): nanoexpressApp;
-  patch(path: string, ...fns: Function<HttpRequest, HttpResponse>[]): nanoexpressApp;
-  del(path: string, ...fns: Function<HttpRequest, HttpResponse>[]): nanoexpressApp;
-  options(path: string, ...fns: Function<HttpRequest, HttpResponse>[]): nanoexpressApp;
-  head(path: string, ...fns: Function<HttpRequest, HttpResponse>[]): nanoexpressApp;
-  trace(path: string, ...fns: Function<HttpRequest, HttpResponse>[]): nanoexpressApp;
-  any(path: string | Function<HttpRequest, HttpResponse> | Function<HttpRequest, HttpResponse>[], ...fns?: Function<HttpRequest, HttpResponse>[]): nanoexpressApp;
-  ws(path: string, ...fns: Function<HttpRequest, HttpResponse>[]): nanoexpressApp;
-  listen(port: number, host?: string): Promise<nanoexpressApp>;
-  close(): boolean;
-  define(prefix: string, routes: AppRoutes?): nanoexpressApp;
-  config: AppConfig
-}
-}
-
-declare function nanoexpress(options?: nanoexpress.AppOptions): nanoexpress.nanoexpressApp;
+declare function nanoexpress(
+  options?: nanoexpress.AppOptions
+): nanoexpress.nanoexpressApp;
 
 export = nanoexpress;
