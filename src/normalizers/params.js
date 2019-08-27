07ce0bfbfadc14b030e5df910c794aa647a72cfa
Davlatjon Shavkatov <dalisoft@mail.ru>
2019-08-27 14:11:03 +0500

    improve params performance

--- src/normalizers/params.js
+++ src/normalizers/params.js
@@ -1,20 +1,11 @@
-const PARAMS_REGEX = /:([A-Za-z0-9_-]+)/g;
-
-export default (req) => {
-  const { rawPath } = req;
-
+export default (req, matches) => {
   let params;
-  if (rawPath.indexOf(':') !== -1) {
-    const paramsMatch = rawPath.match(PARAMS_REGEX);
-
-    if (paramsMatch) {
-      if (!params) {
-        params = {};
-      }
-      for (let i = 0, len = paramsMatch.length; i < len; i++) {
-        const name = paramsMatch[i];
-        params[name.substr(1)] = req.getParameter(i);
-      }
+  if (matches) {
+    if (!params && matches.length > 0) {
+      params = {};
+    }
+    for (let i = 0, len = matches.length; i < len; i++) {
+      params[matches[i]] = req.getParameter(i);
     }
   }
 
