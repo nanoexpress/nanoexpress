1b59b427a359dddfa9e07d7939c1c944f825c0ad
Davlatjon Shavkatov <dalisoft@mail.ru>
2019-12-22 14:46:41 +0500

    add file-upload to packed middlewares export

--- src/packed/middlewares/index.js
+++ src/packed/middlewares/index.js
@@ -1,4 +1,5 @@
 export { default as passportInitialize } from './passport.js';
 export { default as reDoc } from './redoc.js';
 export { default as bodyParser } from './body-parser.js';
+export { default as fileUpload } from './file-upload.js';
 export { default as swaggerUi } from './swagger-ui.js';
