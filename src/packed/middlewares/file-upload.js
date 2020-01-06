d3396d055d09ca30dcf14c89df97e1e7a4e4a07d
dalisoft <3511344+dalisoft@users.noreply.github.com>
2020-01-06 14:40:45 +0500

    fix(file-upload): field naming fix

--- src/packed/middlewares/file-upload.js
+++ src/packed/middlewares/file-upload.js
@@ -29,6 +29,11 @@ export default () => {
                 value = body[key];
               }
             }
+            if (value === 'null') {
+              value = null;
+            } else if (value === 'undefined') {
+              value = 'undefined';
+            }
             body[key] = value;
           });
           form.on('file', (key, file, filename, encoding, mime) => {
