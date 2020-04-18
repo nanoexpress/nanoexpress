8828592104634f2da15a0d22b22d955c1c36f0dd
dalisoft <dalisoft@mail.ru>
2020-04-19 01:12:55 +0500

    fix(middlewares): move redoc to separate repo

--- src/packed/middlewares/index.js
+++ src/packed/middlewares/index.js
@@ -1,4 +1,3 @@
-export { default as passportInitialize } from './passport.js';
-export { default as reDoc } from './redoc.js';
 export { default as fileUpload } from './file-upload.js';
+export { default as passportInitialize } from './passport.js';
 export { default as swaggerUi } from './swagger-ui.js';
