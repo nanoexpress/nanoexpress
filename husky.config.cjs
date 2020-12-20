bb08d0eedd6f44b3e4bd14c48ca4b2aa77e6fbf0
dalisoft <dalisoft@mail.ru>
2020-12-20 20:00:29 +0500

    chore: upgrade deps, fix configurations

--- husky.config.cjs
+++ husky.config.cjs
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-extraneous-dependencies, node/no-unpublished-require
 module.exports = require('husky-config-airlight');
 // Our alpha linter
 module.exports.hooks['pre-push'] = 'smartlint';
