ce25a648398c6f75546e159ed8c682d982ebe0e1
dalisoft <dalisoft@mail.ru>
2021-03-11 22:57:48 +0500

    docs: add expirement, closes #165

---
+++ examples/sse.js
@@ -0,0 +1,26 @@
+// eslint-disable-next-line import/no-unresolved, node/no-missing-import
+import sse from 'sse-broadcast';
+import nanoexpress from '../src/nanoexpress.js';
+
+const app = nanoexpress({ swagger: {} });
+const sseApp = sse();
+
+app.use(async (req, res) => {
+  res.socket = { setNoDelay: () => {} };
+  const _oldWrite = res.write;
+  res.write = function resWrite(data, charset, callback) {
+    _oldWrite.call(this, data, charset);
+    callback();
+  };
+});
+app.get('/events', (req, res) => {
+  sseApp.subscribe('channel', res);
+});
+
+app.post('/event/:type', { isRaw: false }, (req, res) => {
+  sseApp.publish('channel', req.params.type, 'whoo! something happened!');
+  res.modifyEnd();
+  res.end();
+});
+
+app.listen(3333);
