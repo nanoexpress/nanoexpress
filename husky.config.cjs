86fa3ac247e50b076ae1e2138211c18fb29078d8
dalisoft <dalisoft@mail.ru>
2021-05-11 06:22:32 +0500

    fix: add husky config, upgrade dependencies

--- husky.config.cjs
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line import/no-extraneous-dependencies, node/no-unpublished-require
-module.exports = require('husky-config-airlight');
-// Our alpha linter
-module.exports.hooks['pre-push'] = 'smartlint';
