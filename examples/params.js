74b3842f6e2b02469b9865882b5ba254d72b26ed
dalisoft <dalisoft@mail.ru>
2021-05-24 07:37:28 +0500

    docs(example): fix example

--- examples/params.js
+++ examples/params.js
@@ -1,7 +1,7 @@
 import nanoexpress from '../src/nanoexpress.js';
 
 nanoexpress()
-  .get()
   .get('/', async () => 'ok')
   .get('/user/:id', (req, res) => res.send({ status: 'ok', user: req.params }))
+  .get('/*', (req, res) => res.send({ path: req.path }))
   .listen(4003);
